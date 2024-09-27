c6581cada3f7e20385f38698d3d535357c60430a
hajak19 <112589564+hajak19@users.noreply.github.com>
2024-09-27 19:43:06 -0400

    placed codes in the correct directories

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -4,6 +4,19 @@ public class Problem1 {
         // TODO: Implement the concatenate method
         // This method should concatenate l2 to l1 and return l1
         // Remember to create new nodes instead of directly linking l1 to l2
+
+        if (l1 == null) {
+            return copyList(l2);
+        }
+    
+        ListNode current = l1;
+        while (current.next != null) {
+            current = current.next;
+        }
+    
+        current.next = copyList(l2);
+        return l1;
+    }
         return null;
     }
 }
--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -1,9 +1,26 @@
-
 public class Problem2 {
     public static ListNode insert(ListNode head, int val, int position) {
-        // TODO: Implement the insert method
-        // This method should insert a new node with the given value at the specified position
-        // If the position is out of bounds, insert the node at the end of the list
-        return null;
+        ListNode newNode = new ListNode(val);
+
+        // If the list is empty or position is 1, insert at the beginning
+        if (head == null || position == 1) {
+            newNode.next = head;
+            return newNode;
+        }
+
+        ListNode current = head;
+        int currentPosition = 1;
+
+        // Traverse the list until we reach the position or the end
+        while (current.next != null && currentPosition < position - 1) {
+            current = current.next;
+            currentPosition++;
+        }
+
+        // Insert the new node
+        newNode.next = current.next;
+        current.next = newNode;
+
+        return head;
     }
 }
