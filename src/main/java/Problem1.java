b68a50545bf1d90d830ebb43689b31c58c75646f
hajak19 <112589564+hajak19@users.noreply.github.com>
2024-09-27 19:48:19 -0400

    Update Problem1.java
    added copyList method

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -1,22 +1,34 @@
 // Problem1.java
 public class Problem1 {
     public static ListNode concatenate(ListNode l1, ListNode l2) {
-        // TODO: Implement the concatenate method
-        // This method should concatenate l2 to l1 and return l1
-        // Remember to create new nodes instead of directly linking l1 to l2
-
         if (l1 == null) {
             return copyList(l2);
         }
-    
+
         ListNode current = l1;
         while (current.next != null) {
             current = current.next;
         }
-    
+
         current.next = copyList(l2);
         return l1;
     }
-        return null;
+
+    // Helper method to copy a list
+    private static ListNode copyList(ListNode list) {
+        if (list == null) {
+            return null;
+        }
+        ListNode newHead = new ListNode(list.val);
+        ListNode current = newHead;
+        list = list.next;
+
+        while (list != null) {
+            current.next = new ListNode(list.val);
+            current = current.next;
+            list = list.next;
+        }
+
+        return newHead;
     }
 }
